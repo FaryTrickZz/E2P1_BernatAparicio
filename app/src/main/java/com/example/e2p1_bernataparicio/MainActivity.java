2f67da5072ea8e0b178b25d08993bf51ba683623
Bernardo aparicio cabot <imojino@hotmail.com>
2020-12-11 15:39:59 +0100

    Cambios

--- app/src/main/java/com/example/e2p1_bernataparicio/MainActivity.java
+++ app/src/main/java/com/example/e2p1_bernataparicio/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.e2p1_bernataparicio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView list = findViewById(R.id.myList);
+
+        String[] options = new String[]{"DONUTS", "FROYO", "GINGERBREAD", "HONEYCOMB", "ICE CREAM", "JELLY BEAN", "KITKAT", "LOLLIPOP"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options);
+
+        list.setAdapter(adapter);
 
     }
 }
